4c4bb45e56c938edf04f0e36b32fbd5ccc659766
Rakov Pavel <p.rakov@elfsight.com>
2023-01-31 21:55:51 +0700

    Настройка css modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { render } from "react-dom";
+import App from "./App";
 import { Counter } from "./components/Counter";
 import { someFn } from "./test";
 
 someFn(213);
 
 render(
-  <div><Counter /></div>,
+  <App />,
   document.getElementById('root')
 )
