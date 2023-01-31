773cda7471150cde745bbe2e9db7d87cf0ec54ce
Rakov Pavel <p.rakov@elfsight.com>
2023-01-31 22:28:25 +0700

    Routing. Code Splitting. Lazy Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './index.scss';
+import { AboutPageLazy } from './pages/aboutPage/AboutPage.lazy';
+import { MainPageLazy } from './pages/mainPage/MainPage.lazy';
 
 const App = () => {
   return (
     <div>
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+            <Route path={'/about'} element={<AboutPageLazy />} />
+            <Route path={'/'} element={<MainPageLazy />}/>
+        </Routes>
+      </Suspense>
     </div>
   )
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { Counter } from "./components/Counter";
 import { someFn } from "./test";
 
 someFn(213);
 
 render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 )
