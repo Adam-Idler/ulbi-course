0943ae126f92366cc4a222361263f9f29a47e61d
Rakov Pavel <p.rakov@elfsight.com>
2023-02-04 00:49:43 +0700

    Начинаем внедрять архитектуру FSD (Feature-Sliced Design)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageLazy } from './pages/aboutPage/AboutPage.lazy';
-import { MainPageLazy } from './pages/mainPage/MainPage.lazy';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,8 +17,8 @@ const App = () => {
       
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-            <Route path='/about' element={<AboutPageLazy />} />
-            <Route path='/' element={<MainPageLazy />}/>
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/' element={<MainPage />}/>
         </Routes>
       </Suspense>
     </div>
